5cdd7ce5a099dfe7ca7be141992c98c1747ad1c0
Rishabh Chawla <rishabhchawla26@gmail.com>
2021-07-20 13:41:12 +0530

    Update accepted_data.py

--- accepted_data.py
+++ accepted_data.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np 
-rom sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler  
 from sklearn.linear_model import LogisticRegression
 
