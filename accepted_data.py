e3026e1a77a94a5efb570399a476aaa727d9af7d
Rishabh Chawla <rishabhchawla26@gmail.com>
2021-07-20 13:52:23 +0530

    Update accepted_data.py

--- accepted_data.py
+++ accepted_data.py
@@ -14,8 +14,8 @@ def loan_status_prediction(loan_amnt,annual_inc,int_rate,term,grade):
     model=LogisticRegression()
 
 
-    X_train=X_train.head(1000).iloc[:,[0,1,2,3,4]].values
-    y_train=y_train.head(1000).iloc[:,[0]].values
+    X_train=X_train.head(1000).loc[:,["loan_amnt","annual_inc","int_rate","term_cat","grade_cat"]].values
+    y_train=y_train.head(1000).iloc[:,["loan_status_cat"]].values
 
     model.fit(X_train,y_train)
     
